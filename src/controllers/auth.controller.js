ab888f65d3384b0f589f924f36e029cbe0fa3e58
Maulidin <mmaulidinabdi@mail.com>
2025-12-17 22:46:32 +0800

    still many err

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,10 +1,28 @@
-export const RegisterUser = (req, res) => {
-  const { username, email, password, fullname } = req.body;
+import * as z from "zod";
+import { prisma } from '../lib/prisma.js';
 
-  res.json({
-    msg: "Register User",
-    data: { username, email, password, fullname },
-  });
+export const RegisterUser = async (req, res) => {
+  try {
+    // validasi
+    const userSchema = z.object({
+      fullname: z.string().min(8, "Fullname minimal 8 karakter"),
+      username: z.string().min(8, "Username minimal 8 karakter"),
+      email: z.email("Gunakan Format Email contoh: example@gmail.com"),
+      password: z.string().min(8, "Password minimal 8 karakter"),
+    });
+    const validate = userSchema.parse(req.body);
+
+    // cek email dan username harus unique
+    // const emailExist = await prisma.user.findUnique({
+    //   where:{
+    //     email: validate.email
+    //   }
+    // })
+
+    // hash password
+
+    // simpan data ke database
+  } catch (err) {}
 };
 
 export const LoginUser = (req, res) => {
