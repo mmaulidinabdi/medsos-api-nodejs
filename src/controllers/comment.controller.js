c1f6dcf7f9c53ad217e641532f578fdfaa266900
Maulidin <mmaulidinabdi@mail.com>
2025-12-27 11:33:33 +0800

    delete comment and change schema(add model Like and add onDelete:cascade on each model)

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -49,7 +49,59 @@ export const createComment = async (req, res) => {
       message: "Comment berhasil dibuat",
       data: newComment,
     });
-    
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      success: false,
+      message: `Something went wrong on server: ${err}`,
+    });
+  }
+};
+
+export const deleteCommentById = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const comment = await prisma.comment.findUnique({
+      where: {
+        id: Number(id),
+      },
+    });
+
+    if (!comment) {
+      return res.status(404).json({
+        success: false,
+        message: "Comment tidak ditemukan",
+      });
+    }
+
+
+    if (comment.userId !== Number(req.user.id)) {
+      return res.status(404).json({
+        success: false,
+        message: "Anda tidak bisa menghapus komentar user lain",
+      });
+    }
+
+    await prisma.comment.delete({
+      where: {
+        id: Number(id),
+      },
+    });
+
+    await prisma.post.update({
+      where: {
+        id: comment.postId,
+      },
+      data: {
+        commentCount: { decrement: 1 },
+      },
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "Comment berhasil dihapus",
+    });
   } catch (err) {
     console.log(err);
     return res.status(500).json({
